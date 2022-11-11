e2b86096f15b53d3cd617d15e874c70afd15f1a1
Raiden Shogun <80555498+ProRaiden@users.noreply.github.com>
2022-11-11 16:54:56 +0530

    Add files via upload

--- scale.c
+++ scale.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 
-void translate(int x, int y, int tr_x, int tr_y, int j) {
+void scale(int x, int y, int sca_x, int sca_y, int j) {
     int new[2];
-    new[0] = x + tr_x;
-    new[1] = y + tr_y;
+    new[0] = x * sca_x;
+    new[1] = y * sca_y;
     printf(">> [%d](%d, %d)\n", j, new[0], new[1]);
 }
 
 int main() {
-    int tr_x, tr_y, i;
+    int sca_x, sca_y, i;
 
     printf("Enter number of coordinates: ");
     scanf("%d", &i);
@@ -22,10 +22,10 @@ int main() {
     }
 
     printf("Enter translation unit (x, y): ");
-    scanf("%d %d", &tr_x, &tr_y);
+    scanf("%d %d", &sca_x, &sca_y);
 
     for (j = 0; j < i; j++) {
-        translate(x[j], y[j], tr_x, tr_y, j);
+        scale(x[j], y[j], sca_x, sca_y, j);
     }
 
     return 0;
